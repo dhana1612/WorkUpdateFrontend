8c04d0df4472fc081fd333bf92e7edca28ec00c4
Dhana <dhana121622@gmail.com>
2025-01-13 04:25:13 +0530

    commi

--- src/EmployeeRole.js
+++ src/EmployeeRole.js
@@ -1,7 +1,16 @@
+//This will call 1st when the page loads
+window.onload = function () {
+    myFunction();
+};
+
+function myFunction() {
+     Retrive_UserName_Role()
+}
+
 async function Retrive_UserName_Role()
 {
     try {
-        const response = await fetch(`https://workupdate.onrender.com/api/Admin/Retrive_UserName_Role`);
+        const response = await fetch(`https://localhost:7035/api/Admin/Retrive_UserName_Role`);
         
         if (!response.ok) {
             const errorMessage = await response.text();
@@ -11,6 +20,7 @@ async function Retrive_UserName_Role()
         const data = await response.json();
 
         if (Array.isArray(data)) {
+            console.log(data)
            // populateTable(data); 
         } else {
             alert("Unexpected data format");
